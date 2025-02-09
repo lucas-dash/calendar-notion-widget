ec05321f4e8de234eaa6923dc77a7818d6650096
lucas-dash <homolka.lukas10@gmail.com>
2025-02-09 12:42:38 +0100

    hide mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import { ModeToggle } from '@/components/ui/mode-toggle';
 export default function Home() {
   return (
     <div className="bg-background">
-      <ModeToggle />
+      <div className="hidden">
+        <ModeToggle />
+      </div>
       <main className="min-h-screen flex items-center justify-center">
         <section className="w-max">
           <MonthCalendar />
